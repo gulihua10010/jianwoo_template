ecaaf804e713f22bde54a5a8a872119d3a0ade19
gulihua <1729846470@qq.com>
2024-04-23 20:26:02 +0800

    init

--- jianwoo-common/src/main/java/cn/jianwoo/common/constant/ConfigConstants.java
+++ jianwoo-common/src/main/java/cn/jianwoo/common/constant/ConfigConstants.java
@@ -117,4 +117,117 @@ public class ConfigConstants {
      * 上传目录
      */
     public static final String QINIUYUN_CONTEXT = "qiniuyun_context";
+
+
+
+    /**
+     * 微信小程序APP ID
+     */
+    public static final String WX_NIMI_PROGRAM_APP_ID = "wx_nimi_program_app_id";
+
+    /**
+     * 微信小程序APP Secret
+     */
+    public static final String WX_NIMI_PROGRAM_APP_SECRET = "wx_nimi_program_app_secret";
+
+    /**
+     * 微信服务号APP ID
+     */
+    public static final String WX_SVC_ACC_APP_ID = "wx_svc_acc_app_id";
+
+    /**
+     * 微信服务号APP Secret
+     */
+    public static final String WX_SVC_ACC_APP_SECRET = "wx_svc_acc_app_secret";
+
+    /**
+     * 微信商户号
+     */
+    public static final String WX_PAY_MERCHANT_ID = "wx_pay_merchant_id";
+
+    /**
+     * 微信商户API私钥路径
+     */
+    public static final String WX_PAY_PRIVATE_KEY_PATH = "wx_pay_private_key_path";
+
+    /**
+     * 微信商户证书序列号
+     */
+    public static final String WX_PAY_MCH_SERIAL_NO = "wx_pay_mch_serial_no";
+
+    /**
+     * 微信支付API V3密钥
+     */
+    public static final String WX_PAY_API_V3_KEY = "wx_pay_api_v3_key";
+
+    /**
+     * 微信支付JSAPI下单下单接口地址
+     */
+    public static final String WX_PAY_TRANSACTIONS_JSAPI_URL = "wx_pay_transactions_jsapi_url";
+
+    /**
+     * 微信支付H5下单下单接口地址
+     */
+    public static final String WX_PAY_TRANSACTIONS_H5_URL = "wx_pay_transactions_h5_url";
+
+    /**
+     * 微信支付Native下单下单接口地址
+     */
+    public static final String WX_PAY_TRANSACTIONS_NATIVE_URL = "wx_pay_transactions_native_url";
+
+    /**
+     * 微信支付查询订单接口地址
+     */
+    public static final String WX_PAY_ORDER_QUERY_URL = "wx_pay_order_query_url";
+    /**
+     * 微信支付异步回调地址
+     */
+    public static final String WX_PAY_NOTIFY_URL = "wx_pay_notify_url";
+
+    /**
+     * H5 页面启用支付宝支付
+     */
+    public static final String ALI_PAY_ENABLE = "ali_pay_enable";
+
+    /**
+     * 支付宝appid
+     */
+    public static final String ALI_PAY_APP_ID = "ali_pay_app_id";
+
+    /**
+     * 支付宝应用私钥
+     */
+    public static final String ALI_PAY_PRIVATE_KEY = "ali_pay_private_key";
+
+    /**
+     * 支付宝应用公钥
+     */
+    public static final String ALI_PAY_APP_PUBLIC_KEY = "ali_pay_app_public_key";
+
+    /**
+     * 支付宝公钥
+     */
+    public static final String ALI_PAY_ALIPAY_PUBLIC_KEY = "ali_pay_alipay_public_key";
+
+    /**
+     * 支付宝网关地址
+     */
+    public static final String ALI_PAY_GATEWAY_URL = "ali_pay_gateway_url";
+
+    /**
+     * 支付宝异步通知地址
+     */
+    public static final String ALI_PAY_NOTIFY_URL = "ali_pay_notify_url";
+
+    /**
+     * 支付宝页面跳转地址
+     */
+    public static final String ALI_PAY_RETURN_URL = "ali_pay_return_url";
+
+    /**
+     * 支付宝页面退出地址
+     */
+    public static final String ALI_PAY_QUIT_URL = "ali_pay_quit_url";
+
+
 }
